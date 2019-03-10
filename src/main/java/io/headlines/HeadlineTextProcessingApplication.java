34f0540217bb447f838618d5a0a7671c5674f0f9
lakshmikanth <kanth.explorer@gmail.com>
2019-03-11 00:40:37 +0800

    Initial Commit

---
+++ src/main/java/io/headlines/HeadlineTextProcessingApplication.java
@@ -0,0 +1,12 @@
+package io.headlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlineTextProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadlineTextProcessingApplication.class, args);
+    }
+}
