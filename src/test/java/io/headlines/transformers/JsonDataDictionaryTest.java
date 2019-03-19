e5920aed40526570ca4d0d259a80e91cfc8ae0a3
lakshmikanth <kanth.explorer@gmail.com>
2019-03-19 23:32:44 +0530

    Use WuManber algorithm for pattern matching in a string

--- src/test/java/io/headlines/transformers/JsonDataDictionaryTest.java
+++ src/test/java/io/headlines/transformers/JsonDataDictionaryTest.java
@@ -1,18 +1,15 @@
 package io.headlines.transformers;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static io.headlines.service.JsonDataDictionaryService.searchForMentionsAndTransform;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JsonDataDictionaryTest {
-
-    Logger log = LoggerFactory.getLogger(JsonDataDictionaryTest.class);
 
     @Test
     public void test_Assert_Country_Match(){
@@ -26,14 +23,13 @@ public class JsonDataDictionaryTest {
         String transformedString_Actual =
                 searchForMentionsAndTransform(textToken,dictionaryTokens);
 
-        log.info("transformedString_Actual is: {}",transformedString_Actual);
-
         //then
         assertNotNull(transformedString_Actual);
+        assertEquals("air nz strike Australia to affect Australiatravellers",transformedString_Actual);
     }
 
     @Test
-    public void test_Assert_CaseInsensitive_Country_Match(){
+    public void test_Assert_CaseInsensitive_Country_Match() {
 
         //given
         String textToken = "air nz strike australia and jApAN to affect iNDiaN and australian travellers";
@@ -44,14 +40,10 @@ public class JsonDataDictionaryTest {
 
         //when
         String transformedString_Actual =
-                searchForMentionsAndTransform(textToken,dictionaryTokens);
-
-        log.info("transformedString_Actual is: {}",transformedString_Actual);
+                searchForMentionsAndTransform(textToken, dictionaryTokens);
 
         //then
         assertNotNull(transformedString_Actual);
+        assertEquals("air nz strike Australia and Japan to affect IndiaN and Australian travellers", transformedString_Actual);
     }
-
-    //
-
 }
